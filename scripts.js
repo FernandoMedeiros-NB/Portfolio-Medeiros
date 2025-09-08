2bfc51f7ce560687549963f430ffd9b8d7a45a0f
Fernando Medeiros <fernando.prado090108@gmail.com>
2025-09-08 18:13:56 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,2 +1,21 @@
-window.open('URL', '_blank')
+function revealOnScroll() {
+    const reveals = document.querySelectorAll(".reveal");
 
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 100;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+// roda 1 vez quando a página abre
+window.addEventListener("load", revealOnScroll);
+
+// roda sempre que rolar a página
+window.addEventListener("scroll", revealOnScroll);
